71c71a234d90b10a27522a7267b3f65e0ee23aa2
RaphaFang <fangsihyu0211@gmail.com>
2024-07-22 16:51:11 +0800

    fix: route bug

--- myapp/task_urls.py
+++ myapp/task_urls.py
@@ -4,5 +4,5 @@ from myapp.weekly_task_api import api_upload
 
 urlpatterns = [
     path('w1/', views.w1, name='w1'),
-    path('/api/upload', api_upload, name='api_upload'),
+    path('task/v1/api/upload', api_upload.upload_task, name='api_upload'),
 ]
