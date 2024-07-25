ca9646111ef7523ac23394418b354432f0b688d9
RaphaFang <fangsihyu0211@gmail.com>
2024-07-25 16:27:27 +0800

    fix: delete AWS_DEFAULT_ACL

--- project/settings.py
+++ project/settings.py
@@ -96,7 +96,7 @@ AWS_S3_REGION_NAME = 'us-east-1'
 STATIC_LOCATION = 'static'
 STATIC_URL = f'{CLOUDFRONT_DOMAIN}/{STATIC_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_DEFAULT_ACL = 'public-read'
+# AWS_DEFAULT_ACL = 'public-read' # 新的預設都沒有
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
