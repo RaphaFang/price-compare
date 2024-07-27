0fe3274920fb3a224d37352f2d772e8dbc490da3
RaphaFang <fangsihyu0211@gmail.com>
2024-07-27 15:45:43 +0800

    fix: add hashlib to meet the requirement

--- myapp/commerce_api/api_ebay.py
+++ myapp/commerce_api/api_ebay.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.conf import settings
+import hashlib
+
 
 
 @csrf_exempt
@@ -16,4 +18,17 @@ def account_deletion_notification(request):
         body = json.loads(request.body)
         print("Received notification:", body)
         return JsonResponse({"message": "Notification received"})
+    
+    elif request.method == 'GET':
+        challenge_code = request.GET.get('challenge_code')
+        if not challenge_code:
+            return HttpResponseForbidden('Forbidden')
+        
+        verification_token = settings.EBAY_VERIFICATION_TOKEN
+        endpoint = "https://raphaelfang.com/pc/v1/ebay/deletion/"
+        combined_string = challenge_code + verification_token + endpoint
+        hashed_value = hashlib.sha256(combined_string.encode()).hexdigest()
+        
+        return JsonResponse({"challengeResponse": hashed_value})
+    
     return HttpResponseForbidden('Forbidden')
