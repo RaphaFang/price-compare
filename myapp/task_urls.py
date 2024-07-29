c964bf4ae6f0db585dc4556b6ae9984113275df6
RaphaFang <fangsihyu0211@gmail.com>
2024-07-29 12:17:40 +0800

    add: a html and a router for loading test

--- myapp/task_urls.py
+++ myapp/task_urls.py
@@ -7,8 +7,8 @@ from myapp.weekly_task_api.api_download import DownloadTask
 # 這個是處理/task的路由機制
 urlpatterns = [
     path('', views.w1, name='w1'),
-    # path('api/upload', api_upload.upload_task, name='api_upload'),
 
     path('api/upload', UploadTask.as_view(), name='upload_task'),
     path('api/download', DownloadTask.as_view(), name='download_task'),
+    # path(''),
 ]
--- project/urls.py
+++ project/urls.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from myapp import views
+
 
 # project 是直接全局控制，所以是要引入我在myapp的檔案
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pc/v1/', include('myapp.pc_urls')),
     path('task/v1/', include('myapp.task_urls')),
+    path('loaderio-55a2eea74ec89c95f0c9f9a4628ecd51.html', views.loading_test, name='loading_test'), 
+
 ]
 
 # 掛載css js的位置，不會與admin衝突
